a59c09a45aae44b83b04b75c5873c6bb89bfaabf
vqlai <695850246@qq.com>
2017-03-20 22:11:05 +0800

    add new file

---
+++ src/components/footer/v_footer.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="v_footer" id="contract">
+    <h1>{{msg}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'v_footer',
+  data () {
+    return {
+      msg: 'Personal Resume Design by cisco lai'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .v_footer{height: 2rem;width: 100%;margin: 0 auto;background-color: #333333;}
+  h1{font-size:0.3rem;color: #fff;text-align: center;line-height: 2rem;}
+</style>
